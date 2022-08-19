bcde66a4404116e9fd7c13eaa6761d96494bc6c3
vonrimS <mr.mytab@gmail.com>
2022-08-20 00:17:03 +0200

    local storage usage added

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import React, { useState, useEffect } from 'react';
 import data from './data';
 import Article from './Article';
 
+const getStorageTheme = () => {
+  let theme = 'light-theme';
+  if (localStorage.getItem('theme')) {
+    theme = localStorage.getItem('theme');
+  }
+  return theme;
+};
+
 function App() {
-  const [theme, setTheme] = useState('light-theme');
+  const [theme, setTheme] = useState(getStorageTheme());
 
   const toggleTheme = () => {
     if (theme === 'light-theme') {
@@ -11,10 +19,12 @@ function App() {
     } else {
       setTheme('light-theme');
     }
+    console.log({ theme });
   };
 
   useEffect(() => {
     document.documentElement.className = theme;
+    localStorage.setItem('theme', theme);
   }, [theme]);
 
   return <main>
