3211b6d61978f8b81158f81d6075f49598cb9bbb
vonrimS <mr.mytab@gmail.com>
2022-08-19 23:50:25 +0200

    light to dark switcher added

--- src/Article.js
+++ src/Article.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import moment from 'moment';
 const Article = ({ title, snippet, date, length }) => {
-    console.log({ date });
-
+    // console.log({ date });
     return <article className="post">
         <h2>{title}</h2>
         <div className="post-info">
-            <span>date</span>
+            <span>{moment(date).format('MMMM dddd Do, YYYY')}</span>
             <span>{length} min read</span>
         </div>
         <p>{snippet}</p>
