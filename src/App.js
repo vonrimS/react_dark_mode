a6109f718f51f55f5a2450b519baf873bcd80151
vonrimS <mr.mytab@gmail.com>
2022-08-19 19:24:02 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+import data from './data';
+import Article from './Article';
+
+function App() {
+  return <main>
+    <nav>
+      <div className="nav-center">
+        <h1>overreacted</h1>
+        <button className="btn">toggle</button>
+      </div>
+      <section className="articles">
+        {data.map((item) => {
+          return <Article key={item.id} {...item} />;
+        })}
+      </section>
+    </nav>
+  </main>;
+}
+
+export default App;
